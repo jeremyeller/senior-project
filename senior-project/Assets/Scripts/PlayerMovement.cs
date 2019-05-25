8f7f300751f88996089fd64d2ab1d2814e63fbb8
Eller <ellerj@countryday.net>
2019-05-25 16:47:01 -0400

    Initial Commit

---
+++ senior-project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D playerRigidbody;
+    private Vector3 velocity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        velocity = Vector3.zero;
+        velocity.x = Input.GetAxisRaw("Horizontal");
+        velocity.y = Input.GetAxisRaw("Vertical");
+        if (velocity != Vector3.zero)
+        {
+            MoveCharacter();
+        }
+    }
+
+    void MoveCharacter()
+    {
+        playerRigidbody.MovePosition(transform.position + velocity * speed * Time.deltaTime);
+    }
+}
