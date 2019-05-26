94f9532347ea757d5846b402f434852198b83d26
Jeremy <thelordofthings235@gmail.com>
2019-05-26 11:29:29 -0400

    cameramovement and playercontroller

---
+++ senior-project/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public GameObject followTarget;
+    private Vector3 targetPosition;
+    public float moveSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        targetPosition = new Vector3(followTarget.transform.position.x, followTarget.transform.position.y, transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+    }
+}
